cdc41e22c403e69b903842f05b3c49d951429fe0
furnivall <danielfurnivall@gmail.com>
2022-10-27 16:09:32 +0100

    Update time_restrict.sql

--- time_restrict.sql
+++ time_restrict.sql
@@ -1,15 +1,20 @@
 CREATE OR REPLACE FUNCTION time_restrict()
     RETURNS TABLE
-            (LIKE sdp_mat_view)
+--     our implementation was focused to control access to a materialized view but feel free to utilise any view or table you like here.
+-- if the LIKE statement is not used, you will need to include each column name individually
+            (LIKE your_mat_view_here)
+--  The below line allows the function executor to inherit the permissions of the creator (within the scope of the function alone).
     SECURITY DEFINER
 as
 $$
     BEGIN
+--     logic can be altered for your given date parameters using postgres date functions. 
+-- Importantly, the solution works based on the local time of the postgres instance (including timezone)
     IF extract(hour FROM date_trunc('hour', now())) BETWEEN 16 and 20 THEN
         RETURN;
     ELSE
         RETURN QUERY
-            SELECT * from sdp_mat_view;
+            SELECT * from your_mat_view_here;
     END IF;
     END
 -- $$ LANGUAGE plpgsql;
